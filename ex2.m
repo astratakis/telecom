b639a5395643779c17071d2d29a2ec3af5e008d3
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-11-10 13:55:17 +0200

    push

--- ex2.m
+++ ex2.m
@@ -1,67 +1,85 @@
 clear all;
 close all;
 
-% A1
+% =========================== A1 ===========================
 
+% ------ < variables > ------
 T = 0.01;
 over = 10;
 Ts = T/over;
 A = 4;
-
-[phi_1, t1] = srrc_pulse(T, over, A, 0);
+a = [0, 0.5, 1];
+% ---------------------------
 
 figure(1);
-subplot(3, 1, 1);
 
-plot(t1, phi_1);
+for index = 1:length(a)
+    [phi(:, index), time(:, index)] = srrc_pulse(T, over, A, a(index));
+    plot(time(:, index), phi(:, index));
+    hold on;
+end
 
-[phi_2, t2] = srrc_pulse(T, over, A, 0.5);
+legend('a = 0', 'a = 0.5', 'a = 1');
+xlabel('t (sec)');
+ylabel('\phi(t)', 'FontSize', 17);
+title('SRRC pulses');
+hold off;
 
-subplot(3, 1, 2);
+%print('-f1', 'srrc_pulses', '-dpng');
 
-plot(t2, phi_2);
+% ==========================================================
 
-[phi_3, t3] = srrc_pulse(T, over, A, 1);
+% =========================== A2 ===========================
 
-subplot(3, 1, 3);
+% ------ < variables > ------
+Fs = 1/Ts;  % Ts = T/over = 0.01/10 = 0.001
+Nf = 1024;  % Chosen Nf
+% ---------------------------
 
-plot(t3, phi_3);
+freq_axis = -Fs/2:Fs/Nf:Fs/2 - Fs/Nf;
 
-
-%A2
-
-Fs = 1000;
-DT = 1/Fs;
-Nf = 2048;
-
-F = [-Fs/2:Fs/Nf:Fs/2 - Fs/Nf];
-
-PHI_1 = fftshift(fft(phi_1, Nf) * DT);
-PHI_2 = fftshift(fft(phi_2, Nf) * DT);
-PHI_3 = fftshift(fft(phi_3, Nf) * DT);
+for index = 1:length(a)
+    PHI(:, index) = fftshift(fft(phi(:, index), Nf) * Ts);
+end
 
 figure(2);
+for index = 1:length(a)
+    plot(freq_axis, abs(PHI(:, index)).^2);
+    hold on;
+end
 
-subplot(3, 1, 1);
-plot(F, abs(PHI_1).^2);
+legend('a = 0', 'a = 0.5', 'a = 1');
+title('Energy spectrum of \phi(t) SRRC pulses');
+ylabel('|\Phi(F)|^2');
+xlabel('F(Hz)');
+hold off;
 
-subplot(3, 1, 2);
-plot(F, abs(PHI_2).^2);
-
-subplot(3, 1, 3);
-plot(F, abs(PHI_3).^2);
+%print('-f2', 'energy_spectrum_srrc_lin', '-dpng');
 
 figure(3);
 
-semilogy(F, abs(PHI_1).^2);
-hold on;
-semilogy(F, abs(PHI_2).^2);
-hold on;
-semilogy(F, abs(PHI_3).^2);
-hold on;
+for index = 1:length(a)
+    semilogy(freq_axis, abs(PHI(:, index)).^2);
+    hold on;
+end
+
+legend('a = 0', 'a = 0.5', 'a = 1');
+title('Energy spectrum of \phi(t) SRRC pulses');
+ylabel('|\Phi(F)|^2');
+xlabel('F(Hz)');
+hold off;
+
+%print('-f3', 'energy_spectrum_srrc_log', '-dpng');
+
+stop;
+
 c = ones(1, length(F))*(T/1000);
 semilogy(F, c);
 hold off;
+
+
+% ==========================================================
+
 % A3
 
 BW = [(1 + 0)/(2*T), (1 + 0.5)/(2*T), (1 + 1)/(2*T)]
