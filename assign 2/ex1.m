cde185fff6bb6e9c84451a01bdb49d461981267d
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-12-05 14:29:03 +0200

    Finished A5

--- assign 2/ex1.m
+++ assign 2/ex1.m
@@ -92,7 +92,7 @@ title('Px(F) Log');
 grid on;
 
 % ---- < variables > ----
-K = 10;                   % The number of random bits
+K = 1000;                   % The number of random bits
 % -----------------------
 
 % Repeat A2 K times...
@@ -117,7 +117,7 @@ for i = 1:K
 end
 
 % Just calculate the average...
-average_power = total_power / K;
+average_power = total_power ./ K;
 
 % Plot the results.
 figure(5);
@@ -128,3 +128,130 @@ legend('Estimated', 'Theoretical');
 xlabel('F (Hz)');
 ylabel('Sx(F)');
 title('Spectral Power Density');
+hold off;
+
+% The difference between K=10 and K=10000 is extreme
+% The accuracy increases with K.
+
+%%%%%%%%%%%%%%%%% < A4 > %%%%%%%%%%%%%%%%%
+
+% ---- < variables > ----
+N = 100;                    % The number of random bits
+K = 500;
+% -----------------------
+
+% Create a random stream of bits.
+bits = (sign(randn(2, N/2)) + 1)/2;
+
+% Create Xn sequence
+Xn = bits_to_4PAM(bits);
+
+% Generate X wave function
+[X, t_x] = generate_x_function(Xn, phi, t, T, over);
+
+figure(6);
+plot(t_x, X);
+xlabel('t (sec)');
+ylabel('X(t)');
+title('X(t) Function');
+
+% Fourier of X wave function
+FOUR_X = fftshift(fft(X, Nf) * Ts);
+
+% Calculate total time
+T_total = t_x(end) - t_x(1);
+
+PX = (abs(FOUR_X).^2)/T_total;
+
+total_power = 0;
+
+for i = 1:K
+    
+    % Generate random bits and calculate Xn stream of symbols
+    bits = (sign(randn(2, N/2)) + 1)/2;
+    Xn = bits_to_4PAM(bits);
+    
+    [X, t_x] = generate_x_function(Xn, phi, t, T, over);
+    
+    FOUR_X = fftshift(fft(X, Nf) * Ts);
+    
+    T_total = t_x(end) - t_x(1);
+    
+    PX = (abs(FOUR_X).^2)/T_total;
+    
+    total_power = total_power + PX;
+end
+
+average_power = total_power ./ K;
+theoretical_power = (var(Xn)/T).*(abs(PHI).^2);
+
+figure(7);
+semilogy(NFFT, average_power);
+hold on;
+semilogy(NFFT, theoretical_power);
+xlabel('F (Hz)');
+ylabel('Sx(F)');
+legend('Estimated', 'Theoretical');
+title('Spectral Power Density');
+
+%%%%%%%%%%%%%%%%% < A5 > %%%%%%%%%%%%%%%%%
+
+N = 100;
+K = 500;
+
+[phi, t] = srrc_pulse(2*T, 2*over, A, a);
+PHI = fftshift(fft(phi, Nf) * Ts);
+
+bits = (sign(randn(1, N)) + 1)/2;
+Xn = bits_to_2PAM(bits);
+
+[X, t_x] = generate_x_function(Xn, phi, t, 2*T, 2*over);
+
+FOUR_X = fftshift(fft(X, Nf) * Ts);
+T_total = t_x(end) - t_x(1);
+PX = (abs(FOUR_X).^2)/T_total;
+
+figure(8);
+subplot(2, 1, 1);
+plot(NFFT, PX);
+xlabel('F (Hz)');
+ylabel('Px(F)');
+title('Px(F) Linear');
+subplot(2, 1, 2);
+semilogy(NFFT, PX);
+xlabel('F (Hz)');
+ylabel('Px(F)');
+title('Px(F) Log');
+
+% Repeat K times in order to calculate an average value...
+
+total_power = 0;
+
+for i = 1:K
+   
+    bits = (sign(randn(1, N)) + 1)/2;
+    Xn = bits_to_2PAM(bits);
+    
+    [X, t_x] = generate_x_function(Xn, phi, t, 2*T, 2*over);
+    
+    FOUR_X = fftshift(fft(X, Nf) * Ts);
+    
+    T_total = t_x(end) - t_x(1);
+    
+    PX = (abs(FOUR_X).^2)/T_total;
+    
+    total_power = total_power + PX;
+end
+
+average_power = total_power ./ K;
+theoretical_power = (var(Xn)/(2*T)).*(abs(PHI).^2);
+
+figure(9);
+semilogy(NFFT, average_power);
+hold on;
+semilogy(NFFT, theoretical_power);
+hold off;
+xlabel('F (Hz)');
+ylabel('Sx(F)');
+title('Spectral Power Density');
+legend('Estimated', 'Theoretical');
