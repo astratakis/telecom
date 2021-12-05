239c2c4203bae4b3ff0bf22ba27ef5e755d1aac4
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-12-05 15:58:37 +0200

    minor change to 4PAM function

--- assign 2/bits_to_4PAM.m
+++ assign 2/bits_to_4PAM.m
@@ -1,6 +1,6 @@
 function Xn = bits_to_4PAM(bits)
     
-    for i = 1:length(bits)
+    for i = 1:length(bits(1, :))
         
         if (bits(1, i) == 0 && bits(2, i) == 0)
             Xn(i) = 3;
