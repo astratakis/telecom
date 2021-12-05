79ccaf41438102a04faf24519673d514926becb9
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-12-05 11:56:02 +0200

    Implemented X wave function

---
+++ assign 2/generate_x_function.m
@@ -0,0 +1,14 @@
+function [X, x_t] = generate_x_function(Xn, phi, phi_t, T, over)
+
+    Ts = T/over;
+    N = length(Xn);
+    
+    x_t_axis = [0:Ts:T];
+    
+    Xd = 1/Ts * upsample(Xn, over);
+    td = [0:Ts:N*T - Ts];
+    
+    X = Ts * conv(Xd, phi);
+    
+    x_t = linspace(phi_t(1) + td(1), phi_t(end) + td(end), length(X));
+end
