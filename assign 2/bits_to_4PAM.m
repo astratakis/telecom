77a012118499534bb5254a7d753f4b8ffc9d9f4b
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-12-05 14:19:13 +0200

    Created bits to 4PAM function

---
+++ assign 2/bits_to_4PAM.m
@@ -0,0 +1,17 @@
+function Xn = bits_to_4PAM(bits)
+    
+    for i = 1:length(bits)
+        
+        if (bits(1, i) == 0 && bits(2, i) == 0)
+            Xn(i) = 3;
+        elseif (bits(1, i) == 0 && bits(2, i) == 1)
+            Xn(i) = 1;
+        elseif (bits(1, i) == 1 && bits(2, i) == 0)
+            Xn(i) = -1;
+        elseif (bits(1, i) == 1 && bits(2, i) == 1)
+            Xn(i) = -3;
+        else
+            disp('ERROR');
+        end
+    end
+end
